b535c033fccef0dc22dd33b84b67d4a5bbad18a5
a153884673 <153884673@qq.com>
2022-10-31 14:27:15 +0800

    feat(mock): 新增大屏管理列表mock数据

--- src/pages/visualizations/screen/index.tsx
+++ src/pages/visualizations/screen/index.tsx
@@ -1,42 +1,116 @@
-import { ActionType, PageContainer, ProFormSwitch, ProList } from '@ant-design/pro-components';
-import { Button, Progress, Tag } from 'antd';
-import { useRef, useState } from 'react';
-
-const data = [
-  '语雀的天空',
-  'Ant Design',
-  '蚂蚁金服体验科技',
-  'TechUI',
-  'TechUI 2.0',
-  'Bigfish',
-  'Umi',
-  'Ant Design Pro',
-].map((item) => ({
-  title: item,
-  subTitle: <Tag color="#5BD8A6">语雀专栏</Tag>,
-  actions: [<a key="run">编辑</a>, <a key="delete">删除</a>],
-  avatar: 'https://gw.alipayobjects.com/zos/antfincdn/UCSiy1j6jx/xingzhuang.svg',
-  content: (
-    <div
-      style={{
-        flex: 1,
-      }}
-    >
-      <div
-        style={{
-          width: 200,
-        }}
-      >
-        <div>发布中</div>
-        <Progress percent={80} />
-      </div>
-    </div>
-  ),
-}));
-
+import screenSrc from '@/assets/screenItem/moke-20211219181327.png';
+import {
+  CopyOutlined,
+  DeleteOutlined,
+  DownloadOutlined,
+  EditOutlined,
+  EllipsisOutlined,
+  EyeOutlined,
+  FormOutlined,
+  SendOutlined,
+} from '@ant-design/icons';
+import { ActionType, PageContainer, ProList } from '@ant-design/pro-components';
+import { Button, Dropdown, Image, Menu, MenuProps, message, Tag, Tooltip } from 'antd';
+import { useRef } from 'react';
 const Screen: React.FC = () => {
-  const [ghost, setGhost] = useState<boolean>(false);
   const actionRef = useRef<ActionType>();
+
+  const onClick: MenuProps['onClick'] = ({ key }) => {
+    message.info(`Click on item ${key}`);
+  };
+
+  const menu = (
+    <Menu
+      onClick={onClick}
+      items={[
+        {
+          label: '预览',
+          key: '1',
+          icon: <EyeOutlined />,
+        },
+        {
+          label: '复制',
+          key: '2',
+          icon: <CopyOutlined />,
+        },
+        {
+          label: '重命名',
+          key: '3',
+          icon: <FormOutlined />,
+        },
+        {
+          type: 'divider',
+        },
+        {
+          label: '发布',
+          key: '4',
+          icon: <SendOutlined />,
+        },
+        {
+          label: '下载',
+          key: '5',
+          icon: <DownloadOutlined />,
+        },
+        {
+          type: 'divider',
+        },
+        {
+          label: '删除',
+          key: '6',
+          icon: <DeleteOutlined />,
+        },
+      ]}
+    />
+  );
+
+  const screenList = [
+    {
+      id: 1,
+      title: '物料1-假数据不可用',
+      release: true,
+      label: '官方案例',
+    },
+    {
+      id: 2,
+      title: '物料2-假数据不可用',
+      release: false,
+      label: '官方案例',
+    },
+    {
+      id: 3,
+      title: '物料3-假数据不可用',
+      release: false,
+      label: '官方案例',
+    },
+    {
+      id: 4,
+      title: '物料4-假数据不可用',
+      release: false,
+      label: '官方案例',
+    },
+    {
+      id: 5,
+      title: '物料5-假数据不可用',
+      release: false,
+      label: '官方案例',
+    },
+  ].map((item) => ({
+    id: item.id,
+    title: item.title,
+    subTitle: (
+      <Tag color={item.release ? '#34c749' : '#fcbc40'}>{item.release ? '已发布' : '未发布'}</Tag>
+    ),
+    actions: [
+      <Tooltip title="编辑" key="edit">
+        <EditOutlined />
+      </Tooltip>,
+      <Dropdown overlay={menu} key="dropdown">
+        <EllipsisOutlined />
+      </Dropdown>,
+    ],
+    avatar: 'https://gw.alipayobjects.com/zos/antfincdn/UCSiy1j6jx/xingzhuang.svg',
+    content: <Image src={screenSrc} height={180} alt={item.title} />,
+  }));
   return (
     <PageContainer>
       <div
@@ -46,26 +120,15 @@ const Screen: React.FC = () => {
           padding: 24,
         }}
       >
-        <ProFormSwitch
-          label="幽灵模式"
-          fieldProps={{
-            checked: ghost,
-            onChange: (e) => setGhost(e),
-          }}
-        />
         <ProList<any>
-          ghost={ghost}
           actionRef={actionRef}
-          itemCardProps={{
-            ghost,
-          }}
           pagination={{
             defaultPageSize: 8,
             showSizeChanger: false,
           }}
           showActions="always"
           rowSelection={{}}
-          grid={{ gutter: 16, column: 2 }}
+          grid={{ gutter: 16, column: 4 }}
           onItem={(record: any) => {
             return {
               onMouseEnter: () => {
@@ -88,7 +151,7 @@ const Screen: React.FC = () => {
           }}
           toolBarRender={() => [<Button>创建大屏</Button>]}
           headerTitle="大屏管理"
-          dataSource={data}
+          dataSource={screenList}
         />
       </div>
     </PageContainer>
