822ede18b405e2b2d382d081d938006fcdfba23f
a153884673 <153884673@qq.com>
2022-11-10 10:48:00 +0800

    feat(setting): 新增settin部分配置,新增charteditModel

--- src/pages/visualizations/screen/detail/components/ChartsItemBox/index.tsx
+++ src/pages/visualizations/screen/detail/components/ChartsItemBox/index.tsx
@@ -1,21 +1,34 @@
+import { EditCanvasTypeEnum } from '@/models/chartEditStore/chartEditStore';
 import type { ConfigType } from '@/packages/index.d';
 import '@/styles/scrollStyle.less';
 import { CSSProperties, memo, useMemo } from 'react';
 import { useDrag } from 'react-dnd';
+import { useDispatch } from 'umi';
 
 import './index.less';
 
 const ChartsItemBox: React.FC<{
   menuOptionsItem: ConfigType;
 }> = ({ menuOptionsItem }) => {
+  const setEditCanvasDispatch = useDispatch();
+
   const [{ isDragging }, drag] = useDrag({
-    // type: menuOptionsItem?.key,
     item: { ...menuOptionsItem, type: menuOptionsItem?.key },
     begin: () => {
-      console.log('start');
+      setEditCanvasDispatch({
+        type: 'chartEditStore/setEditCanvas',
+        payload: {
+          [EditCanvasTypeEnum.IS_CREATE]: true,
+        },
+      });
     },
     end: () => {
-      console.log('end');
+      setEditCanvasDispatch({
+        type: 'chartEditStore/setEditCanvas',
+        payload: {
+          [EditCanvasTypeEnum.IS_CREATE]: false,
+        },
+      });
     },
     collect: (monitor) => ({
       isDragging: monitor.isDragging(),
@@ -34,7 +47,6 @@ const ChartsItemBox: React.FC<{
     <div className="charts-item-box">
       <div className="charts-item-box-header">
         <div>123</div>
-        {/* <mac-os-control-btn :mini="true" :disabled="true"></mac-os-control-btn> */}
         <div>{menuOptionsItem?.title}</div>
       </div>
       <div className="charts-item-box-img" ref={drag} style={{ ...containerStyle }}>
